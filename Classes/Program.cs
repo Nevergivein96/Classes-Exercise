13558478c00f9af21f5735fe08b48e356eeb35cd
Spencer Hansen <spencerhansen@gmail.com>
2022-12-31 12:57:38 -0700

    finished first exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var dodge = new Car();
+
+            dodge.Make = "Dodge";
+            dodge.Model = "1500";
+            dodge.Year = 2003;
+
+            var ford = new Car()
+            {
+                Make = "Ford",
+                Model = "F-150",
+                Year = 1998,
+            };
+
+            var carList = new List<Car>() { dodge, ford};
+
+            foreach(var car in carList)
+            {
+                Console.WriteLine($"{car.Make} {car.Model} {car.Year}");
+            }
+            
         }
     }
 }
